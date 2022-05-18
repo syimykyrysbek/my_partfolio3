11d7880ccba55a46a2812667f7834d914f03ce1e
syimykyrysbek <syimykyrysbek@gmail.com~>
2022-05-18 15:33:08 +0600

    added static files, css and images

--- my_partfolio3/urls.py
+++ my_partfolio3/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage
+# from django.conf import settings
+# from django.conf.urls.static import static
+
 urlpatterns = [
     path('', homepage, name='homepage' ),
     path('admin/', admin.site.urls),
-]
+] 
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
